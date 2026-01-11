9487983a34a6c0345c5ef1986610f1a1d352ca45
Dark Master <152901560+darkmaster0345@users.noreply.github.com>
2026-01-11 23:22:43 +0500

    Upgrade AGP to 9.0.0-alpha01 and modify Kotlin plugin
    Updated Android Gradle Plugin version and commented out Kotlin plugin line.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.application") version "9.0.0-alpha01" apply false // Use your actual AGP version
+    // Remove or comment out the line below:
+    // id("org.jetbrains.kotlin.android") version "2.0.21" apply false 
+    
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.28" apply false
 }
